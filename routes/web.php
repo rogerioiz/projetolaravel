cc4f5f8cef6214033a4b4b3f34f61e81808debfd
Rogério Izidorio Santos Junior <izidoriorogerio@gmail.com>
2019-07-24 17:43:04 -0300

    redirecionamento do adm

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
  Route::get('/index', function(){
      return view('painel.index');
- });
+ })->middleware('checkadmin');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
